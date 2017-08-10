d1132a7621df0584f47692f17d19db04b8371240
Mario Sánchez <mr.mario.sm@gmail.com>
2017-08-10 20:12:13 +0200

    Implement value object Email for Affiliate entity

--- app/src/ParkimeterAffiliates/AffiliateBundle/CompilerPass/DoctrineMappingCompilerPass.php
+++ app/src/ParkimeterAffiliates/AffiliateBundle/CompilerPass/DoctrineMappingCompilerPass.php
@@ -2,18 +2,11 @@
 
 namespace ParkimeterAffiliates\AffiliateBundle\CompilerPass;
 
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class DoctrineMappingCompilerPass implements CompilerPassInterface
 {
-    public function __construct()
-    {
-        if (false === class_exists(DoctrineBundle::class, true)) {
-            throw new \Exception(sprintf("You are required to have %s installed.", DoctrineBundle::class));
-        }
-    }
 
     /**
      * @param ContainerBuilder $container
@@ -21,23 +14,12 @@ class DoctrineMappingCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $config = $container->getExtensionConfig('doctrine');
-        $mergedConfig = $this->mergedDoctrineArray($config);
-
-        $container->prependExtensionConfig('doctrine', $mergedConfig);
-    }
-
-    /**
-     * @param array $config
-     * @return array
-     */
-    private function mergedDoctrineArray(array $config)
-    {
-        $merged = [];
+        $mergedConfig = [];
 
         foreach ($config as $configArray) {
-            $merged = array_merge_recursive($merged, $configArray);
+            $mergedConfig = array_merge_recursive($mergedConfig, $configArray);
         }
 
-        return $merged;
+        $container->prependExtensionConfig('doctrine', $mergedConfig);
     }
 }
